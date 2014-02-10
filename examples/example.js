cd4b3df00b1b5851e14dda88d9d9656639e0a8a5
Javidan Quliyev <q.cavidan@gmail.com>
2014-02-10 15:30:39 +0400

    fixed example some bugfixes

--- examples/example.js
+++ examples/example.js
@@ -1,7 +1,16 @@
+var beyazperde = require('beyazperde')
+
 beyazperde.search("terminator 2", function(searchError, list){
-  if(!err){
+  if(!searchError){
       beyazperde.movieInfo(list.id, function(infoError, info){
-         success.call(self, err, info)
+         if(!infoError){
+            console.log('--info--')
+            console.log(info)
+         }else{
+            console.log('--error--')
+            console.log(infoError)
+         }
+         
       })
   }else{
       console.log('--error--')
